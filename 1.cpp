9d8d0dd680ab324803a9d3e0501323aa32aa8af9
supriya759694 <supriyabhowmick321@gmail.com>
2024-07-06 13:01:38 +0530

    add cube

--- 1.cpp
+++ 1.cpp
@@ -3,11 +3,14 @@
  using namespace std;
  int main()
  {
-     int x,y;
+     int x,y,cube;
      cout<<"Enter a number";
      cin>>x;
      y=x*x;
+    cube= x*x*x;
      cout<<"square of" <<x <<"is "<<y;
+     cout<<"cube of" <<x <<"is "<<cube;
+
      cout<<endl;
      return 0;
  }
