2719e313a170988d5a1a7af522f8ea9a7cdcd529
supriya759694 <supriyabhowmick321@gmail.com>
2024-07-06 13:08:43 +0530

    new file added

---
+++ 1a.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+     int x,y;
+     cout<<"Enter a number";
+     cin>>x;
+     y=x*x;
+    cube= x*x*x;
+     cout<<"square of" <<x <<"is "<<y;
+     
+     cout<<endl;
+     return 0;
+ }
